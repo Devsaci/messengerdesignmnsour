7222d0f0940a94677d227c8d108280e5c825331c
saci zakaria <sacideveloppeur@gmail.com>
2021-12-09 19:24:40 +0100

    NetworkImage

--- lib/messager_screen.dart
+++ lib/messager_screen.dart
@@ -11,8 +11,11 @@ class MessengerScreen extends StatelessWidget {
         elevation: 10,
         title: Row(
           children: [
-            CircleAvatar(),
-            SizedBox(width: 20.0),
+            CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/46720185?s=400&u=1448d38406436f4c8ababbe8811bd691e9488489&v=4"),
+            ),
+            SizedBox(width: 30.0),
             Text(
               "Chat",
               style: TextStyle(fontSize: 20, color: Colors.black87),
