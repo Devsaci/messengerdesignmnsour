cc7180c602a22614f228ba1d5091f60a883fbbd2
saci zakaria <sacideveloppeur@gmail.com>
2021-12-09 08:55:59 +0100

    Row / CircleAvatar, Text

--- lib/messager_screen.dart
+++ lib/messager_screen.dart
@@ -1,6 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -11,8 +9,15 @@ class MessengerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("MessengerScreen"),
+        backgroundColor: Colors.white,
         elevation: 10,
+        title: Row(
+          children: [
+            CircleAvatar(),
+            Text("MessengerScreen"),
+          ],
+        ),
+
       ),
     );
   }
